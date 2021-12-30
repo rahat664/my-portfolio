a754fe769b1dc58f47c5d36955f52921238dd8ae
rahat <rahatkabir20@gmail.com>
2021-12-30 22:33:36 +0600

    feat:service added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AuthorService} from "./Service/author.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'portfolio';
+  constructor(private authorService: AuthorService) {
+  }
+  ngOnInit(): void {
+    this.authorService.getAuthor()
+      .subscribe( author => {
+        console.log(author.name)
+      })
+  }
 }
