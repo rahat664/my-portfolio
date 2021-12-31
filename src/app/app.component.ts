ad3faa2fc24802720b2e935f297f44a469ddad69
rahat <rahatkabir20@gmail.com>
2021-12-31 19:09:36 +0600

    feat:added slider

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,13 @@ import {AuthorService} from "./Service/author.service";
 })
 export class AppComponent implements OnInit{
   title = 'portfolio';
+  images = [
+    { src: "https://rb.gy/ohx0bd" },
+    { src: "https://rb.gy/gggxy8" },
+    { src: "https://rb.gy/z2a0fy" },
+    { src: "https://rb.gy/nsefjh" },
+    { src: "https://rb.gy/dssu2a" }
+  ];
   constructor(private authorService: AuthorService) {
   }
   ngOnInit(): void {
